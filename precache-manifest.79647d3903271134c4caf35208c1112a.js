bb13640743cfe9d1ca1f382721aebb4748a38894
Headgie <headgy@yandex.ru>
2020-01-07 01:43:37 +0300

    Updates

---
+++ precache-manifest.79647d3903271134c4caf35208c1112a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41cf0dadf392e010b7c93e927d572e2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "79deb5a111ff4560d48b",
+    "url": "/monsters-rolodex/static/css/main.cd5ff10f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79deb5a111ff4560d48b",
+    "url": "/monsters-rolodex/static/js/main.f508d98b.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
